954daa5e30a2c12490ad2571fc998bcf12220a9d
stevenarimu@gmail.com <stevenarimu@gmail.com>
2024-02-28 06:33:16 +0900

    toast commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import Navbar from "./components/nav/Navbar";
 import Footer from "./components/footer/footer";
 import CartProvider from "@/providers/CartProvider";
+import { Toaster } from "react-hot-toast";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -22,6 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className}text-slate-700`}>
+        <Toaster toastOptions={{
+          style: {
+            background: 'rgb(51 65 85)',
+            color: '#fff',
+          }
+        }} />
         <CartProvider>
           <div className="flex flex-col min-h-screen">
             <Navbar />
